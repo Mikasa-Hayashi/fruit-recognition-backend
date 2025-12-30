a123c9e6aeca0ffbee9b37405830321e73f0458c
Ivan <ivandenisovich2005@gmail.com>
2025-12-30 13:14:40 +0300

    chore: fix typo in variable name

--- src/config.py
+++ src/config.py
@@ -2,11 +2,11 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABAE_URL: str
+    DATABASE_URL: str
     MODEL_PATH: str
 
     class Config:
         env_file = '.env'
-        
+
 
 settings = Settings()
