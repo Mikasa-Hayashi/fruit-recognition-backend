70851fcdb44652ce0307af7ff343d825b208b93e
Ivan <ivandenisovich2005@gmail.com>
2025-12-31 17:52:51 +0300

    fix: change time format to wo timezone

--- src/models/fruits.py
+++ src/models/fruits.py
@@ -7,5 +7,5 @@ class Fruit(FruitBase, table=True):
     __tablename__ = 'fruits'
 
     id: Optional[int] = Field(default=None, primary_key=True)
-    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc).replace(tzinfo=None))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc).replace(tzinfo=None))
